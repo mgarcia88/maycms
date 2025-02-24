9871b5e487cdf63e61fdca9f75f66b1dbb4ffc00
maiara <maiaracamargo@gmail.com>
2025-02-24 19:37:31 -0300

    feat: implementing content model and tests

--- _test/content_test.go
+++ _test/content_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/mgarcia88/maycms/domain"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,4 +20,17 @@ func ShouldReturnErrorWhenUUIDIsNotValid(t *testing.T) {
 
 	err := content.Validate()
 	require.Error(t, err)
+}
+
+func ShouldReturnNilWhenUUIDIsValid(t *testing.T) {
+	content := domain.NewContent()
+	content.ID = uuid.NewV4().String()
+	content.Title = "Post de teste"
+	content.ContentText = "Lorem ipsum"
+	content.Status = "Ativo"
+	content.CreatedAt = time.Now()
+	content.UpdatedAt = time.Now()
+
+	err := content.Validate()
+	require.Nil(t, err)
 }
