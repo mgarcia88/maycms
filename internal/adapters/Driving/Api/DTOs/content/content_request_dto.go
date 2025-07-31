747fb4f14035888579afc2f7430b62decd0ba95c
maiara <maiaracamargo@gmail.com>
2025-07-30 22:14:06 -0300

    chore: refactoring dtos

--- internal/adapters/Driving/Api/DTOs/content/content_request_dto.go
+++ internal/adapters/Driving/Api/DTOs/content/content_request_dto.go
@@ -1,6 +1,6 @@
 package DTO
 
-type ContentRequestBody struct {
+type ContentRequest struct {
 	Title       string `json:"Title" binding:"required,min=10,max=100"`
 	ContentText string `json:"ContentText" binding:"required,min=20,max=255"`
 	Status      string `json:"Status" binding:"required,min=5,max=15"`
